6dbc2a11ebd1eac64e1a7a0a652862a0ef7fff01
Zak <zamp07@gmail.com>
2019-01-14 23:04:07 -0600

    Added a Recipes screen. I'm not sure if I like it though because it doesn't have the same whole menu, so to switch to Nutrition you have to go to the home screen first.

--- app/src/main/java/beer/unacceptable/unacceptablehealth/MainActivity.java
+++ app/src/main/java/beer/unacceptable/unacceptablehealth/MainActivity.java
@@ -1,5 +1,6 @@
 package beer.unacceptable.unacceptablehealth;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -82,10 +83,12 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_exercise) {
             // Handle the camera action
+
         } else if (id == R.id.nav_nutrition) {
 
         } else if (id == R.id.nav_recipes) {
-
+            Intent i = new Intent(getApplicationContext(), Recipes.class);
+            startActivity(i);
         } else if (id == R.id.nav_goals) {
 
         }
