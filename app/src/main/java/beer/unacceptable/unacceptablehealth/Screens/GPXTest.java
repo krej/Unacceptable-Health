b71d9c00e9a41e9722a000e5a85d15d721819210
Zak <zamp07@gmail.com>
2020-10-05 20:55:56 -0500

    I don't know what this commit is, its huge. It happened during the AndroidX conversion, this might be what broke some stuff... I don't know.

---
+++ app/src/main/java/beer/unacceptable/unacceptablehealth/Screens/GPXTest.java
@@ -0,0 +1,15 @@
+package beer.unacceptable.unacceptablehealth.Screens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import beer.unacceptable.unacceptablehealth.R;
+
+public class GPXTest extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_g_p_x_test);
+    }
+}
